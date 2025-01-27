f651d344a5a943d94765c87b54387601b9eaa5ce
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-01-27 12:53:06 +1100

    Connect Apollo Client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 import './index.css'
 import App from './App.tsx'
 
+const client = new ApolloClient({
+    uri: import.meta.env.VITE_QUOTE_SERVER_ENDPOINT,
+    cache: new InMemoryCache(),
+})
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ApolloProvider client={client}>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </ApolloProvider>,
 )
