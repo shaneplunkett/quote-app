743cf7d62ab166a2268a8db45fa0b21de5c43c5b
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-01-27 14:32:25 +1100

    Add Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+import LoginPage from './pages/LoginPage'
+import NotFound from './pages/NotFound'
 import './index.css'
-import App from './App.tsx'
 
 const client = new ApolloClient({
     uri: import.meta.env.VITE_QUOTE_SERVER_ENDPOINT,
     cache: new InMemoryCache(),
 })
 
+const router = createBrowserRouter([{
+    path: '/',
+    element: <LoginPage />,
+    errorElement: <NotFound />
+}])
+
 createRoot(document.getElementById('root')!).render(
     <ApolloProvider client={client}>
         <StrictMode>
-            <App />
+            <RouterProvider router={router} />
         </StrictMode>
     </ApolloProvider>,
 )
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,6 @@
+
+function LoginPage() {
+    return <h1>Hello World</h1>
+}
+
+export default LoginPage;
